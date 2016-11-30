925d23a19dd84d0420b2067b2c9661bd12dd9a66
John <pwnerjohn@gmail.com>
2016-11-30 02:20:10 -0800

    better handling of irc server responses (read: still really bad and needs much more work)

--- ircbot.py
+++ ircbot.py
@@ -18,6 +18,8 @@ class IRCProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         print('IRC: Connection made!')
         self.transport = transport
+        self.login()
+        self.irc_connected()
 
     def data_received(self, data):
         data = data.decode()
@@ -28,25 +30,48 @@ class IRCProtocol(asyncio.Protocol):
         self.relay_to_discord("IRC: Connection to the server has been lost.")
 
     def handle_data(self, data):
-        # the first message received means a connection was successful
-        # commence login operations
-        if self.connected is False:
-            self.login()
-            self.irc_connected()
-            self.connected = True
-
-        #TODO: actually handle login response
-        # hacky ping/pong response for now...
-
         message = format(data)
 
-        print(message)
 
-        if self.discord_connected:
-            self.relay_to_discord(message)
+        # unfortunately data comes in blocks sometimes, which means this method
+        # will not work unless we can set up some kind of list that handles
+        # data split by \r\n per element while also being able to receive more
+        # data by appending it to the end of the list
+        # TODO: ^
+
+        if not message.startswith(":"):
+            self.handle_server_message(message)
+
+        # so :nick!user@host COMMAND :text is what irc sends us
+        # if command is a number e.g. 001-999 then it's a server rpl response
+
+        elif (message.split()[1].isdigit()):
+            self.handle_server_rpl(message.split()[1])
+        # otherwise it's a message from an irc service or user
+        else:    
+            if self.discord_connected:
+                self.relay_to_discord(message)
+                print("recv: {}".format(message))
+            else:
+                # TODO: handle messages when connected to IRC but not discord
+                pass
+   
+    def handle_server_message(self, message):
+
+        # TODO: need a better way to handle server messages -- HIGH PRIORITY
 
         if message.startswith("PING"):
+            print("PING recv: {}".format(message))
             self.send("PONG :" + message.split(":",1)[1])
+        
+        else:
+            print("IRC: Received unknown message from server: {}".format(message))
+
+    def handle_server_rpl(self, rpl):
+        # TODO: need better way of handling rpl codes
+        if value_rpl == '001':
+            print("RPL_WELCOME Received.")
+           
     
     def send(self, message):
         print("IRC: Sending: %s" % (message))
